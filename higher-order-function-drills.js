dc1e05d27d454cd5c618644d39cc9d0aabd09630
Mantong Zhao <mantong.cn@gmail.com>
2019-06-04 11:49:16 -0700

    warnings drill3

--- higher-order-function-drills.js
+++ higher-order-function-drills.js
@@ -45,3 +45,28 @@ const filteredNames = filter(myNames, function(name) {
 
 console.log(filteredNames) // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
+
+
+function hazardWarningCreator (typeOfWarning){
+  let warningCounter=0;
+  return function(location){
+    warningCounter++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+  }
+
+}
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const rainWarning = hazardWarningCreator('Rain on the Road');
+const tornadoWarning = hazardWarningCreator('Tornado on the Road');
+
+rocksWarning('Main St and Pacific Ave');
+// => DANGER There is a Rocks on the Road hazard at Main St and Pacific Ave
+// => The Rocks on the Road hazard has triggered 1 time(s) today!
+
+rocksWarning('Centinela Ave and Olympic Blvd');
+// => DANGER There is a Rocks on the Road hazard at Centinela Ave and Olympic Blvd
+// => The Rocks on the Road hazard has triggered 2 time(s) today!
+
+// Invoke the other hazard creators you instantiated, too
+
